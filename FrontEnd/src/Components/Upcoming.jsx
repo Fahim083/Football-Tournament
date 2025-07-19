fe66a31c26fe666e697fca747c08b0481e22f236
Fahim083 <fahimhaowlader07@gmail.com>
2025-07-20 02:48:00 +0600

    player search section almost done

--- FrontEnd/src/Components/Upcoming.jsx
+++ FrontEnd/src/Components/Upcoming.jsx
@@ -25,12 +25,12 @@ const Upcoming = () => {
         <h3 className='text-xm text-gray-100'>Tournamet Name</h3>
         <h4 className='text-sm text-gray-100'>{match.matchCategory}</h4>
         </div>
-        <div className='flex justify-between items-center mt-5'>
+        <div className='flex justify-between items-center mt-5 mx-4'>
         <img  src={logo} alt="firstTeam" className='W-10 h-10 ' />
         <h3 className='text-xm text-gray-200'>{match.matchDate}</h3>
         <img src={logo} alt="secondTeam" className='W-10 h-10 ' />
         </div>
-        <div className='flex justify-between items-center mt-2 px-1'>
+        <div className='flex justify-between items-center mt-2 px-1 mx-4'>
           <h2 className='text-lg w-8'>WCL</h2>  {/* This is a placeholder for the firsr team short name */}
           <h2 className='text-lg'>{match.matchTime}</h2>
          <h2 className='text-lg w-8'>BPL</h2>     {/* This is a placeholder for the second team short name */}
--- FrontEnd/src/Pages/PlayerPage.jsx
+++ FrontEnd/src/Pages/PlayerPage.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+
 
 const PlayerPage = () => {
   return (
     <div>
-      i am the palyer page
+      <div className=" py-5">
+        <div className="flex gap-4 items-center justify-between  mb-6 w-1/2">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full text-lg font-semibold  p-3 border rounded-md border-gray-300 focus:outline-none  focus:border-orange-500"
+          />
+          <div className="flex justify-end gap-4" >
+          <select className="w-1/3  text-lg font-semibold  p-3 border rounded-md text-gray-400 border-gray-300 focus:outline-none  focus:border-orange-500">
+            <option>All Dates</option>
+            <option>July 2025</option>
+            <option>August 2025</option>
+          </select>
+          <div>
+          <button className="w-full md:w-auto inline px-6 py-3 bg-[rgb(71,144,222)] text-lg font-bold text-white rounded-md cursor-pointer hover:bg-orange-600  transition-colors">
+            SEARCH
+          </button>
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PlayerPage
+export default PlayerPage;
